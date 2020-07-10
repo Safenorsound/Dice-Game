e0057fe7fdd7cd6eec8170a4d8ceedaeb1d85e90
Safenorsound <ijroback@gmail.com>
2020-07-10 14:43:46 -0700

    set scores to 0

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ activePlayer = 0;
 
 document.querySelector('.dice').getElementsByClassName.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
 //Anonymous function
 document.querySelector('btn-roll').addEventListener('click', function () {
   // 1. Random Number
