1d9a535c4884bc7fd4e8e29d87497b49f6d6bf37
Safenorsound <ijroback@gmail.com>
2020-07-12 15:23:57 -0700

    hide dice on 1 roll

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ document.querySelector('btn-roll').addEventListener('click', function () {
 
     document.querySelector(.player-0-panel).classList.toggle('active');
     document.querySelector(.player-1-panel).classList.toggle('active');
+    document.querySelector(.dice.style.display = 'none';
     //document.querySelector(.player-0-panel).classList.remove('active');
     //document.querySelector(.player-1-panel).classList.add('active');
   }
