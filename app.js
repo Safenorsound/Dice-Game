f97d8e94bb1bb4b1763dbf0b39e458d4a4eb7655
Safenorsound <ijroback@gmail.com>
2020-07-13 12:14:29 -0700

    update roundScore

--- app.js
+++ app.js
@@ -49,8 +49,6 @@ document.querySelector('btn-roll').addEventListener('click', function () {
     document.querySelector(.player-0-panel).classList.toggle('active');
     document.querySelector(.player-1-panel).classList.toggle('active');
     document.querySelector(.dice.style.display = 'none';
-    //document.querySelector(.player-0-panel).classList.remove('active');
-    //document.querySelector(.player-1-panel).classList.add('active');
   }
 
 });
@@ -58,10 +56,20 @@ document.querySelector('btn-roll').addEventListener('click', function () {
 document.querySelector('.btn-hold').addEventListener('click', function() {
     //Add current score to global score
 scores[activePlayer]; += roundScore;
+
     //Update User Interface
+    document.querySelector('#score-' + activePlayer).textContent = [activePlayer] //Active player update number 0 or 1.
 
     //Check if the player won the game
+    //next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
 
+    document.querySelector(.player-0-panel).classList.toggle('active');
+    document.querySelector(.player-1-panel).classList.toggle('active');
+    document.querySelector(.dice.style.display = 'none';
 
 };
