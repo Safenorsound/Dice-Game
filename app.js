af6f4f03d6707757d025726c8b68ce8fe152ec9a
Safenorsound <ijroback@gmail.com>
2020-07-20 13:04:07 -0700

    line 90 comment about reseting scores

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ function init() {
 
   document.querySelector('.dice').getElementsByClassName.display = 'none';
 
+  //Resets the Scores
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
 
